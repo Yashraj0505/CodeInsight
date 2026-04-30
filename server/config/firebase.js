74a089332b7ac0985438500ac59fc2c1493685c4
Raghav_Mulay <raghav.mulay24@vit.edu>
2026-04-30 07:19:50 +0530

    Fixing upload error

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,13 +1,22 @@
 import admin from "firebase-admin";
+import { createRequire } from "module";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+let credential;
 
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Production (Render): service account stored as env var
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+  credential = admin.credential.cert(serviceAccount);
+} else {
+  // Local development: use serviceAccountKey.json file
+  const require = createRequire(import.meta.url);
+  const serviceAccount = require("./serviceAccountKey.json");
+  credential = admin.credential.cert(serviceAccount);
+}
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+  admin.initializeApp({ credential });
 }
 
 export default admin;
